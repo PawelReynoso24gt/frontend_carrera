b8ca52d8bde630544e7637a34b587ecbbaf9e010
AngelyThC <angelyth321@umes.edu.gt>
2024-11-14 21:27:40 -0600

    arreglando tabla

--- src/component/tipoPago/tipoPago.jsx
+++ src/component/tipoPago/tipoPago.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import { Button, Form, Table, Modal, Alert } from 'react-bootstrap';
+import axios from "axios";
+import { Button, Form, Table, Modal, Alert } from "react-bootstrap";
 
 function TipoPago() {
   const [tiposPago, setTiposPago] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [editingTipoPago, setEditingTipoPago] = useState(null);
-  const [newTipoPago, setNewTipoPago] = useState({ tipo: '', estado: 1 });
+  const [newTipoPago, setNewTipoPago] = useState({ tipo: "", estado: 1 });
   const [showAlert, setShowAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
+  const [alertMessage, setAlertMessage] = useState("");
 
   useEffect(() => {
     fetchTiposPago();
@@ -16,34 +16,34 @@ function TipoPago() {
 
   const fetchTiposPago = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/tipospagos');
+      const response = await axios.get("http://localhost:5000/tipospagos");
       setTiposPago(response.data);
     } catch (error) {
-      console.error('Error fetching tipos de pago:', error);
+      console.error("Error fetching tipos de pago:", error);
     }
   };
 
   const fetchActiveTiposPago = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/tipopago/activas');
+      const response = await axios.get("http://localhost:5000/tipopago/activas");
       setTiposPago(response.data);
     } catch (error) {
-      console.error('Error fetching active tipos de pago:', error);
+      console.error("Error fetching active tipos de pago:", error);
     }
   };
 
   const fetchInactiveTiposPago = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/tipopago/inactivas');
+      const response = await axios.get("http://localhost:5000/tipopago/inactivas");
       setTiposPago(response.data);
     } catch (error) {
-      console.error('Error fetching inactive tipos de pago:', error);
+      console.error("Error fetching inactive tipos de pago:", error);
     }
   };
 
   const handleShowModal = (tipoPago = null) => {
     setEditingTipoPago(tipoPago);
-    setNewTipoPago(tipoPago || { tipo: '', estado: 1 });
+    setNewTipoPago(tipoPago || { tipo: "", estado: 1 });
     setShowModal(true);
   };
 
@@ -61,32 +61,36 @@ function TipoPago() {
     e.preventDefault();
     try {
       if (editingTipoPago) {
-        // Actualizar tipo de pago
-        await axios.put(`http://localhost:5000/tipopagos/${editingTipoPago.idTipoPago}`, newTipoPago);
-        setAlertMessage('Tipo de pago actualizado con éxito');
+        await axios.put(
+          `http://localhost:5000/tipopagos/${editingTipoPago.idTipoPago}`,
+          newTipoPago
+        );
+        setAlertMessage("Tipo de pago actualizado con éxito");
       } else {
-        // Crear nuevo tipo de pago
-        await axios.post('http://localhost:5000/tipopagos/create', newTipoPago);
-        setAlertMessage('Tipo de pago creado con éxito');
+        await axios.post("http://localhost:5000/tipopagos/create", newTipoPago);
+        setAlertMessage("Tipo de pago creado con éxito");
       }
       fetchTiposPago();
       setShowAlert(true);
       handleCloseModal();
     } catch (error) {
-      console.error('Error submitting tipo de pago:', error);
+      console.error("Error submitting tipo de pago:", error);
     }
   };
 
   const toggleEstado = async (id, estadoActual) => {
     try {
-      // Cambiar el estado a su opuesto
       const nuevoEstado = estadoActual === 1 ? 0 : 1;
-      await axios.put(`http://localhost:5000/tipopagos/${id}`, { estado: nuevoEstado });
+      await axios.put(`http://localhost:5000/tipopagos/${id}`, {
+        estado: nuevoEstado,
+      });
       fetchTiposPago();
-      setAlertMessage(`Tipo de pago ${nuevoEstado === 1 ? 'activado' : 'inactivado'} con éxito`);
+      setAlertMessage(
+        `Tipo de pago ${nuevoEstado === 1 ? "activado" : "inactivado"} con éxito`
+      );
       setShowAlert(true);
     } catch (error) {
-      console.error('Error toggling estado:', error);
+      console.error("Error toggling estado:", error);
     }
   };
 
@@ -95,18 +99,54 @@ function TipoPago() {
       <div className="row">
         <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12">
           <div className="crancy-section-title mg-btm-10">
-            <h3 className="crancy-section__title">CRUD</h3>
+            <h3 className="crancy-section__title">Gestión de tipos de pago</h3>
           </div>
         </div>
       </div>
 
-      {/* Botones para Filtrar Tipos de Pago */}
       <div className="container mt-4">
-        <Button variant="primary" onClick={() => handleShowModal()}>Agregar Tipo de Pago</Button>
-        <Button variant="success" className="ml-2" onClick={fetchActiveTiposPago}>Activos</Button>
-        <Button variant="danger" className="ml-2" onClick={fetchInactiveTiposPago}>Inactivos</Button>
+        <Button
+          style={{
+            backgroundColor: "#743D90",
+            borderColor: "#007AC3",
+            padding: "5px 10px",
+            width: "130px",
+            marginRight: "10px",
+          }}
+          onClick={() => handleShowModal()}
+        >
+          Agregar Tipo de Pago
+        </Button>
+        <Button
+          style={{
+            backgroundColor: "#007AC3",
+            borderColor: "#007AC3",
+            padding: "5px 10px",
+            width: "100px",
+            marginRight: "10px",
+          }}
+          onClick={fetchActiveTiposPago}
+        >
+          Activos
+        </Button>
+        <Button
+          style={{
+            backgroundColor: "#009B85",
+            borderColor: "#007AC3",
+            padding: "3px 10px",
+            width: "100px",
+          }}
+          onClick={fetchInactiveTiposPago}
+        >
+          Inactivos
+        </Button>
 
-        <Alert variant="success" show={showAlert} onClose={() => setShowAlert(false)} dismissible>
+        <Alert
+          variant="success"
+          show={showAlert}
+          onClose={() => setShowAlert(false)}
+          dismissible
+        >
           {alertMessage}
         </Alert>
 
@@ -124,12 +164,30 @@ function TipoPago() {
               <tr key={tipoPago.idTipoPago}>
                 <td>{tipoPago.idTipoPago}</td>
                 <td>{tipoPago.tipo}</td>
-                <td>{tipoPago.estado ? 'Activo' : 'Inactivo'}</td>
+                <td>{tipoPago.estado ? "Activo" : "Inactivo"}</td>
                 <td>
-                  <Button variant="warning" onClick={() => handleShowModal(tipoPago)}>Editar</Button>
                   <Button
-                    variant={tipoPago.estado ? "secondary" : "success"}
-                    onClick={() => toggleEstado(tipoPago.idTipoPago, tipoPago.estado)}
+                    style={{
+                      backgroundColor: "#007AC3",
+                      borderColor: "#007AC3",
+                      padding: "3px 10px",
+                      width: "100px",
+                      marginRight: "5px",
+                    }}
+                    onClick={() => handleShowModal(tipoPago)}
+                  >
+                    Editar
+                  </Button>
+                  <Button
+                    style={{
+                      backgroundColor: "#007AC3",
+                      borderColor: "#007AC3",
+                      padding: "3px 10px",
+                      width: "100px",
+                    }}
+                    onClick={() =>
+                      toggleEstado(tipoPago.idTipoPago, tipoPago.estado)
+                    }
                   >
                     {tipoPago.estado ? "Inactivar" : "Activar"}
                   </Button>
@@ -139,10 +197,11 @@ function TipoPago() {
           </tbody>
         </Table>
 
-        {/* Modal para crear y editar tipos de pago */}
         <Modal show={showModal} onHide={handleCloseModal}>
           <Modal.Header closeButton>
-            <Modal.Title>{editingTipoPago ? 'Editar Tipo de Pago' : 'Agregar Tipo de Pago'}</Modal.Title>
+            <Modal.Title>
+              {editingTipoPago ? "Editar Tipo de Pago" : "Agregar Tipo de Pago"}
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Form onSubmit={handleSubmit}>
@@ -168,8 +227,16 @@ function TipoPago() {
                   <option value={0}>Inactivo</option>
                 </Form.Control>
               </Form.Group>
-              <Button variant="primary" type="submit">
-                {editingTipoPago ? 'Actualizar' : 'Crear'}
+              <Button
+                style={{
+                  backgroundColor: "#007AC3",
+                  borderColor: "#007AC3",
+                  padding: "5px 10px",
+                  width: "120px",
+                }}
+                type="submit"
+              >
+                {editingTipoPago ? "Actualizar" : "Crear"}
               </Button>
             </Form>
           </Modal.Body>
