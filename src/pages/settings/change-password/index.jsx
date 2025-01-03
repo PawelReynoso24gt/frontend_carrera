435f5b1ec926cbb32081de36344f42ddab837c05
danipro777 <daniel1603@umes.edu.gt>
2025-01-03 00:24:13 -0600

    Agregando boton para visualizar contrasenia

--- src/pages/settings/change-password/index.jsx
+++ src/pages/settings/change-password/index.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import img from "../../../assets/img/password-reset.png";
 import fetchWithToken from "../../../utils/fetchWithToken";
 import { getUserDataFromToken } from "../../../utils/jwtUtils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import "./index.css";
 
 function ChangePassword() {
   const [formData, setFormData] = useState({
@@ -12,6 +15,11 @@ function ChangePassword() {
 
   const [mensaje, setMensaje] = useState("");
   const [passwordStrength, setPasswordStrength] = useState("");
+  const [showPassword, setShowPassword] = useState({
+    currentPassword: false,
+    newPassword: false,
+    confirmPassword: false,
+  });
 
   // Manejar cambios en los campos del formulario
   const handleInputChange = (e) => {
@@ -45,49 +53,57 @@ function ChangePassword() {
     setPasswordStrength(strength);
   };
 
+  // Alternar visibilidad de la contraseña
+  const toggleShowPassword = (field) => {
+    setShowPassword((prevState) => ({
+      ...prevState,
+      [field]: !prevState[field],
+    }));
+  };
+
   // Manejar el envío del formulario
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Verificar que las contraseñas nuevas coincidan
     if (formData.newPassword !== formData.confirmPassword) {
-        setMensaje("La nueva contraseña y la confirmación no coinciden.");
-        return;
+      setMensaje("La nueva contraseña y la confirmación no coinciden.");
+      return;
     }
 
     try {
-        // Obtener el idUsuario y el token del localStorage
-        const idUsuario = getUserDataFromToken(localStorage.getItem("token"))?.idUsuario;
-        const token = localStorage.getItem("token"); // Obtén el token almacenado
+      // Obtener el idUsuario y el token del localStorage
+      const idUsuario = getUserDataFromToken(localStorage.getItem("token"))?.idUsuario;
+      const token = localStorage.getItem("token"); // Obtén el token almacenado
 
-        if (!idUsuario || !token) {
-            setMensaje("No se encontró el ID del usuario o el token en el almacenamiento local.");
-            return;
-        }
+      if (!idUsuario || !token) {
+        setMensaje("No se encontró el ID del usuario o el token en el almacenamiento local.");
+        return;
+      }
 
-        // Enviar la solicitud al backend con el token en el encabezado
-        const response = await fetchWithToken(`http://localhost:5000/usuarios/${idUsuario}/contrasenia`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`, // Agregar el token al encabezado
-            },
-            body: JSON.stringify({
-                currentPassword: formData.currentPassword,
-                newPassword: formData.newPassword,
-            }),
-        });
+      // Enviar la solicitud al backend con el token en el encabezado
+      const response = await fetchWithToken(`http://localhost:5000/usuarios/${idUsuario}/contrasenia`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // Agregar el token al encabezado
+        },
+        body: JSON.stringify({
+          currentPassword: formData.currentPassword,
+          newPassword: formData.newPassword,
+        }),
+      });
 
-        if (response.ok) {
-            setMensaje("La contraseña ha sido actualizada correctamente.");
-            setFormData({ currentPassword: "", newPassword: "", confirmPassword: "" }); // Limpiar el formulario
-        } else {
-            const error = await response.json();
-            setMensaje(`Error: ${error.message || "Ocurrió un error al actualizar la contraseña."}`);
-        }
+      if (response.ok) {
+        setMensaje("La contraseña ha sido actualizada correctamente.");
+        setFormData({ currentPassword: "", newPassword: "", confirmPassword: "" }); // Limpiar el formulario
+      } else {
+        const error = await response.json();
+        setMensaje(`Error: ${error.message || "Ocurrió un error al actualizar la contraseña."}`);
+      }
     } catch (error) {
-        console.error("Error al actualizar la contraseña:", error);
-        setMensaje("Ocurrió un error al intentar actualizar la contraseña.");
+      console.error("Error al actualizar la contraseña:", error);
+      setMensaje("Ocurrió un error al intentar actualizar la contraseña.");
     }
   };
 
@@ -105,50 +121,92 @@ function ChangePassword() {
               <div className="form-group">
                 <label className="crancy-wc__form-label">Antigua contraseña</label>
                 <div className="form-group__input">
-                  <input
-                    className="crancy-wc__form-input"
-                    placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                    id="current-password"
-                    type="password"
-                    name="currentPassword"
-                    maxLength="16"
-                    required
-                    value={formData.currentPassword}
-                    onChange={handleInputChange}
-                  />
+                  <div className="input-container">
+                    <input
+                      className="crancy-wc__form-input"
+                      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                      id="current-password"
+                      type={showPassword.currentPassword ? "text" : "password"}
+                      name="currentPassword"
+                      maxLength="60"
+                      required
+                      value={formData.currentPassword}
+                      onChange={handleInputChange}
+                    />
+                    <span onClick={() => toggleShowPassword("currentPassword")}
+                      style={{
+                        position: "absolute",
+                        right: "20px",
+                        top: "52%",
+                        transform: "translateY(-50%)",
+                        cursor: "pointer",
+                        fontSize: "18px",
+                        color: "#333",
+                      }}>
+                      <FontAwesomeIcon icon={showPassword.currentPassword ? faEyeSlash : faEye} />
+                    </span>
+                  </div>
                 </div>
               </div>
               <div className="form-group">
                 <label className="crancy-wc__form-label">Nueva contraseña</label>
                 <div className="form-group__input">
-                  <input
-                    className="crancy-wc__form-input"
-                    placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                    id="new-password"
-                    type="password"
-                    name="newPassword"
-                    maxLength="16"
-                    required
-                    value={formData.newPassword}
-                    onChange={handleInputChange}
-                  />
+                  <div className="input-container">
+                    <input
+                      className="crancy-wc__form-input"
+                      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                      id="new-password"
+                      type={showPassword.newPassword ? "text" : "password"}
+                      name="newPassword"
+                      maxLength="60"
+                      required
+                      value={formData.newPassword}
+                      onChange={handleInputChange}
+                    />
+                    <span onClick={() => toggleShowPassword("newPassword")}
+                      style={{
+                        position: "absolute",
+                        right: "20px",
+                        top: "52%",
+                        transform: "translateY(-50%)",
+                        cursor: "pointer",
+                        fontSize: "18px",
+                        color: "#333",
+                      }}>
+                      <FontAwesomeIcon icon={showPassword.newPassword ? faEyeSlash : faEye} />
+                    </span>
+                  </div>
                 </div>
                 {passwordStrength && <p style={{ color: passwordStrength === "La contraseña es fuerte." ? "green" : "red" }}>{passwordStrength}</p>}
               </div>
               <div className="form-group">
                 <label className="crancy-wc__form-label">Confirmar contraseña</label>
                 <div className="form-group__input">
-                  <input
-                    className="crancy-wc__form-input"
-                    placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                    id="confirm-password"
-                    type="password"
-                    name="confirmPassword"
-                    maxLength="16"
-                    required
-                    value={formData.confirmPassword}
-                    onChange={handleInputChange}
-                  />
+                  <div className="input-container">
+                    <input
+                      className="crancy-wc__form-input"
+                      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                      id="confirm-password"
+                      type={showPassword.confirmPassword ? "text" : "password"}
+                      name="confirmPassword"
+                      maxLength="60"
+                      required
+                      value={formData.confirmPassword}
+                      onChange={handleInputChange}
+                    />
+                    <span onClick={() => toggleShowPassword("confirmPassword")}
+                      style={{
+                        position: "absolute",
+                        right: "20px",
+                        top: "52%",
+                        transform: "translateY(-50%)",
+                        cursor: "pointer",
+                        fontSize: "18px",
+                        color: "#333",
+                      }}>
+                      <FontAwesomeIcon icon={showPassword.confirmPassword ? faEyeSlash : faEye} />
+                    </span>
+                  </div>
                 </div>
               </div>
               <div className="crancy__item-button--group crancy__ptabs-bottom">
