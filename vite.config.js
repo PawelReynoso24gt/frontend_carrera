2d9a97bba9f32e325eb1d832972105c3a69dfb17
AngelyThC <angelyth321@umes.edu.gt>
2025-01-15 23:54:39 -0600

    detalles

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "localhost", // Puedes dejarlo como localhost o usar 0.0.0.0
+    port: 5173,        // Cambia el puerto si es necesario
+    open: "/login",    // Agrega esta línea para que la ruta inicial sea "/login"
+  },
 });
